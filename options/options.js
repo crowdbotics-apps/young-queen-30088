9dbb942804e8ac97d76900ae6748e1e25ab4cdcb
Crowdbotics <team@crowdbotics.com>
2021-08-25 00:59:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "young_queen_30088Identifier",
+  url: "https://young_queen_30088Identifier.botics.co",
+  api: "https://young_queen_30088Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
